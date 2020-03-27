554393e368f1dae082cbfcb30c6e0e70ce94d101
Alex Bispo <alexbispo.artes@gmail.com>
2020-03-27 19:53:03 +0000

    Implements New Incident page layout.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default class Routes extends React.Component {
 
@@ -13,6 +14,7 @@ export default class Routes extends React.Component {
           <Route path="/" exact component={Logon} />
           <Route path="/register" component={Register} />
           <Route path="/profile" component={Profile} />
+          <Route path="/incidents/new" component={NewIncident} />
         </Switch>
       </BrowserRouter>
     );
