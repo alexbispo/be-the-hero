06162b643cc87750076fbde8894ce9f33a8c3be9
Alex Bispo <alexbispo.artes@gmail.com>
2020-03-27 19:27:37 +0000

    Iplements Profile page layout.

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
-
+import Profile from './pages/Profile';
 
 export default class Routes extends React.Component {
 
@@ -12,6 +12,7 @@ export default class Routes extends React.Component {
         <Switch>
           <Route path="/" exact component={Logon} />
           <Route path="/register" component={Register} />
+          <Route path="/profile" component={Profile} />
         </Switch>
       </BrowserRouter>
     );
