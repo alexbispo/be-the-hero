bfb75ae39f6fe2143686138efe6f55eeb3188d0b
Alex Bispo <alexbispo.artes@gmail.com>
2020-03-29 10:41:41 +0100

    Starting mobile project. Implements incidents screen layout.

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import styles from './styles';
+import logoImg from '../../assets/logo.png';
+
+export default class Incidents extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={logoImg} />
+          <Text style={styles.headerText}>
+            Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+          </Text>
+        </View>
+
+        <Text style={styles.title}>Bem-vindo!</Text>
+        <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+        <FlatList
+          data={[1,2,3,4,5]}
+          style={styles.incidentList}
+          keyExtractor={incident => String(incident)}
+          renderItem={() => (
+            <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Caso de teste</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={() => {}}>
+                <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                <Feather name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+          )} />
+
+      </View>
+    );
+  }
+}
